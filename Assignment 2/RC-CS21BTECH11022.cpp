7cc9e37192e8dd55d97d25c2fe2a92c697c74e1f
TushitaSharva <tushitasharva03@gmail.com>
2024-03-20 11:36:02 +0530

    Initial Commit

---
+++ Assignment 2/RC-CS21BTECH11022.cpp
@@ -0,0 +1,255 @@
+/*
+    Janga Tushita Sharva
+    CS21BTECH11022
+    Aim: To implement and analyze Roucairol and Carvalho's Algorithm fro MUTEX
+    Algorithm:
+    All processes work induvidually for outCSTime (variable, exponential) seconds
+    Then they would want to enter mutual exclusion
+
+
+
+    - reqSet: a set which contains those it needs to send request and recieve reply from
+            (initially it should be set of all pids lesser than the rank)
+    - defSet : a set containing processes whose messages are deferred
+    - repSet : a set containing processes to whom reply was sent
+
+    REPLY:
+    - Add the requesting process ID to the repSet
+    - send reply message
+
+    REQUEST:
+    Send requests to all elements in the reqSet
+
+    RECEIVE:
+    Keep listening for messages. There can be two types of messages: REPLY, REQUEST
+    - If recieved a REQ message:
+        execute REPLY if:
+            - The process has no unfullfilled request and it is not executing CS
+            - The unfullfilled request has larger timestamp than that of received request
+        Else add to defSet
+    - If recieved a REPLY message:
+            - Remove from the reqSet this process
+            - When reqSet is null, put reqSet = repSet, repSet = null
+            - Execute critical section
+            - After done with critical section, send REPLY to defSet processes. make defSet null.
+*/
+
+#include <iostream>
+#include <set>
+#include <random>
+#include <string.h>
+#include <unistd.h>
+#include <string>
+#include <stdlib.h>
+#include <cstdlib>
+#include <ctime>
+#include <sstream>
+#include <mpi.h>
+#include <time.h>
+#include <chrono>
+#include <fstream>
+#include <atomic>
+#include <mutex>
+#include <pthread.h>
+#define D 1
+#define REQ 1
+#define REP 2
+#define DONE 3
+using namespace std;
+
+class my_data
+{
+public:
+    int pid;
+    int lamport_clock;
+    int size;
+    int total_requests;
+    int requests_sent;
+    int alpha;
+    int beta;
+};
+
+ifstream inputfile;
+set<int> reqSet;
+set<int> defSet;
+set<int> repSet;
+int done;
+bool inCS = false;
+int request_time = -1; // Stores the time of requesting
+bool grantWithMe = false;
+
+// Helper Function: Generates a random number from an exponential distribution with a mean of 'exp_time'.
+double Timer(float exp_time)
+{
+    default_random_engine generate;
+    exponential_distribution<double> distr(1.0 / exp_time);
+    return distr(generate);
+}
+
+void *performer_func(void *args)
+{
+    my_data *data = (my_data *)args;
+
+    while (data->requests_sent < data->total_requests)
+    {
+        request_time = -1;
+        data->lamport_clock += 1;
+        sleep(Timer(data->alpha));
+        request_time = data->lamport_clock;
+
+        if (!grantWithMe)
+        {
+            data->lamport_clock += 1;
+
+            for (auto i : repSet)
+            {
+                reqSet.insert(i);
+            }
+
+            repSet.clear();
+
+            for (auto i : reqSet)
+            {
+                MPI_Send(&data->lamport_clock, 1, MPI_INT, i, REQ, MPI_COMM_WORLD);
+            }
+        }
+
+        else if (grantWithMe)
+        {
+            criticalSection(data);
+        }
+    }
+
+    return (void *)NULL;
+}
+
+void *criticalSection(void *args)
+{
+    my_data *data = (my_data *)args;
+    grantWithMe = true;
+    inCS = true;
+    sleep(Timer(data->beta));
+    inCS = false;
+    data->requests_sent++;
+
+    for (auto i : defSet)
+    {
+        MPI_Send(&data->lamport_clock, 1, MPI_INT, i, REP, MPI_COMM_WORLD);
+        defSet.erase(i);
+    }
+
+    if (data->requests_sent == data->total_requests)
+    {
+        done++;
+        for (int i = 0; i < data->size; i++)
+        {
+            if (i != data->pid)
+            {
+                MPI_Send(&data->lamport_clock, 1, MPI_INT, i, DONE, MPI_COMM_WORLD);
+            }
+        }
+    }
+}
+
+void *reciever_func(void *args)
+{
+    my_data *data = (my_data *)args;
+    while (true)
+    {
+        int recv_msg = 0;
+        MPI_Status status;
+        MPI_Recv(&recv_msg, 1, MPI_INT, MPI::ANY_SOURCE, MPI::ANY_TAG, MPI_COMM_WORLD, &status);
+        data->lamport_clock = max(data->lamport_clock, recv_msg);
+        data->lamport_clock += 1;
+
+        if (status.MPI_TAG == REQ)
+        {
+            if ((request_time == -1 && inCS == false) || (request_time > data->lamport_clock && inCS == false))
+            {
+                grantWithMe = false;
+                MPI_Send(&data->lamport_clock, 1, MPI_INT, status.MPI_SOURCE, REP, MPI_COMM_WORLD);
+                repSet.insert(status.MPI_SOURCE);
+            }
+
+            else
+            {
+                defSet.insert(status.MPI_SOURCE);
+            }
+        }
+
+        else if (status.MPI_TAG == REP)
+        {
+            reqSet.erase(status.MPI_SOURCE);
+
+            if (reqSet.empty() == true)
+            {
+                for (auto i : repSet)
+                {
+                    reqSet.insert(i);
+                    repSet.erase(i);
+                }
+
+                criticalSection(data);
+            }
+        }
+
+        else if (status.MPI_TAG == DONE)
+        {
+            done++;
+        }
+
+        if(done == data->size)
+        {
+            break;
+        }
+    }
+    return (void *)NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    /* Reading paramters starts */
+
+    int n, k, alpha, beta; // Input parameters
+    inputfile.open("inp-params.txt");
+
+    inputfile >> n >> k >> alpha >> beta;
+    inputfile.close();
+
+    /* Reading paramters ends */
+
+    /* MPI INITIALISATION*/
+    int pid, size;
+    int provided;
+    MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &pid);
+
+    /* Initializing reqSet with processes of lower ranks */
+    for (int i = 0; i < pid; i++)
+    {
+        reqSet.insert(i);
+    }
+
+    /*Initialising parameters to be sent over threads */
+    my_data *data = new my_data;
+    data->pid = pid;
+    data->size = size;
+    data->total_requests = k;
+    data->requests_sent = 0;
+    data->lamport_clock = 0;
+    data->alpha = alpha;
+    data->beta = beta;
+
+    pthread_t listener;
+    pthread_t performer;
+
+    pthread_create(&listener, NULL, &reciever_func, data);
+    pthread_create(&listener, NULL, &performer_func, data);
+
+    pthread_join(listener, NULL);
+    pthread_join(performer, NULL);
+    delete data;
+
+    return 0;
+}
